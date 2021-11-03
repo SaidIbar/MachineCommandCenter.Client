6653b5b151a6fdd01a5c3960da40eeb8bc74be4e
saibibarbachane <60187657+saibibarbachane@users.noreply.github.com>
2021-11-03 16:04:09 +0100

    Fixed offline online buttons

--- MachineCommandCenter.Client/Program.cs
+++ MachineCommandCenter.Client/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace MachineCommandCenter.Client
 {
@@ -17,9 +20,15 @@ namespace MachineCommandCenter.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-           // builder.Services.AddHttpClient<IMachineDataService, MachineDataService>(client => client.BaseAddress = new Uri("http://localhost:44321/"));
+            // builder.Services.AddHttpClient<IMachineDataService, MachineDataService>(client => client.BaseAddress = new Uri("http://localhost:44321/"));
             builder.Services.AddHttpClient<IMachineDataService, MachineDataService>(client => client.BaseAddress = new Uri("https://localhost:44393/")); 
             
             await builder.Build().RunAsync();
