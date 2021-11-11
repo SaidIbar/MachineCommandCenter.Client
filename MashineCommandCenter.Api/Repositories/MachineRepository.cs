8fc188e8e45e36a752fd11f0a519e291488b9f9b
saibibarbachane <60187657+saibibarbachane@users.noreply.github.com>
2021-11-11 10:18:25 +0100

    Added animated loader

--- MashineCommandCenter.Api/Repositories/MachineRepository.cs
+++ MashineCommandCenter.Api/Repositories/MachineRepository.cs
@@ -17,7 +17,7 @@ namespace MachineCommandCenter.Api.Repositories
 
         public IEnumerable<Machine> GetAllMachines()
         {
-            var allMachines = _appDbContext.Machines.OrderByDescending(s => s.SentDataDateTime);
+            var allMachines = _appDbContext.Machines.OrderByDescending(s => s.Name);
             return allMachines;
         }
 
